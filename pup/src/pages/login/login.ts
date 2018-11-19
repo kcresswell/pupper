f44dd498a14ca1da14acaeb37ed52f0d656c0f55
kcresswell <makayla.cresswell@gmail.com>
2018-11-19 08:41:45 -0700

    closer on login post request

--- pup/src/pages/login/login.ts
+++ pup/src/pages/login/login.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
-import { Http, Response, URLSearchParams } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -11,35 +11,32 @@ import 'rxjs/add/operator/map';
 })
 export class LoginPage {
 
+  responseData : any;
+
   constructor(public navCtrl: NavController, public http: Http) {
-
-    let loginData = {
-      "username": "test@test.com",
-      "password": "password"
-    };
-
-    let httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
-    
-    // var headers = new Headers();
-    // headers.append('Content-Type', 'application/json' );
-    // const requestOptions = new RequestOptions({ headers: headers });
-
-    console.log('page loaded');
-    return this.http.post('https://pupper.us-east-1.elasticbeanstalk.com/login', loginData, httpHeaders)
-      .subscribe(data => {
-        // console.log(data['_body']);
-        console.log('response received');
-       }, error => {
-        console.log(error);
-      });
+    console.log('Login page constructor');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Login Page');
-  }
+    login(){
+      console.log('Login button clicked');
 
-  loginResponse() {
-    this.navCtrl.push(TabsPage);
-  }
-    
- }
+      let loginData = {
+        "username": "test@test.com",
+        "password": "password"
+      };
+
+      this.http.post('https://pupper.us-east-1.elasticbeanstalk.com/login', loginData, new HttpHeaders().set('Content-Type', 'application/json'))
+      // this.http.post('http://localhost:5000/login', loginData, new HttpHeaders().set('Content-Type', 'application/json'))
+        .subscribe(data => {
+          console.log('response received');
+
+          console.log(data['_body']);
+         }, error => {
+          console.log(error);
+        });
+      //TODO: ONLY DO THIS ACTION IF LOGIN IS SUCCESSFUL
+      // this.navCtrl.push(TabsPage, {});
+
+    }
+
+  }
