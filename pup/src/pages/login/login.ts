bb8f37b5a82fe6d3931d0576f1eaf1617c80db4a
kcresswell <makayla.cresswell@gmail.com>
2018-11-19 20:02:11 -0700

    able to get user input for login page

--- pup/src/pages/login/login.ts
+++ pup/src/pages/login/login.ts
@@ -6,36 +6,54 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+    selector: 'page-login',
+    templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  responseData : any;
+    responseData: any;
+    username: string;
+    password: string; 
 
-  constructor(public navCtrl: NavController, public http: Http) {
-    console.log('Login page constructor');
-  }
 
-    login(){
-      console.log('Login button clicked');
+    constructor(public navCtrl: NavController, public http: Http) {
+        console.log('Login page constructor');
+    }
 
-      let loginData = {
-        "username": "test@test.com",
-        "password": "password"
-      };
 
-       this.http.post('https://pupper.us-east-1.elasticbeanstalk.com/login', loginData)
-       //this.http.post('http://localhost:5000/login', loginData, new HttpHeaders().set('Content-Type', 'application/json'))
-        .subscribe(data => {
-          console.log('response received');
-        //TODO: ONLY DO THIS ACTION IF LOGIN IS SUCCESSFUL
-        this.navCtrl.push(TabsPage, {});
-          console.log(data['_body']);
-         }, error => {
-          console.log(error);
-        });
 
-    }
+    login() {
+        console.log('Login button clicked');
 
-  }
+        let loginDetails = {
+            username: this.username, 
+            password: this.password
+        };
+
+        console.log("USERNAME: " + loginDetails.username); 
+        console.log("PASSWORD: " + loginDetails.password);  
+
+        this.navCtrl.push(TabsPage, {});
+
+        // //   console.log('usernameID', this.usernameID);
+        // //   console.log('passwordID', this.passwordID);
+
+        // let loginData = {
+        //     "username": "test@test.com",
+        //     "password": "password"
+        // };
+
+        // this.http.post('https://pupper.us-east-1.elasticbeanstalk.com/login', loginData)
+        //     //this.http.post('http://localhost:5000/login', loginData, new HttpHeaders().set('Content-Type', 'application/json'))
+        //     .subscribe(data => {
+        //         console.log('response received');
+        //         //TODO: ONLY DO THIS ACTION IF LOGIN IS SUCCESSFUL
+        //         this.navCtrl.push(TabsPage, {});
+        //         console.log(data['_body']);
+        //     }, error => {
+        //         console.log(error);
+        //     });
+
+    }
+
+}
