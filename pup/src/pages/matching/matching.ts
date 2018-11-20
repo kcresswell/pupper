07e76259953d58eba6b33ef4d684fa4d69b94a90
kcresswell <makayla.cresswell@gmail.com>
2018-11-19 20:35:34 -0700

    all input from user being accepted

--- pup/src/pages/matching/matching.ts
+++ pup/src/pages/matching/matching.ts
@@ -1,42 +1,43 @@
-import { Component,EventEmitter } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: 'page-matching',
-  templateUrl: 'matching.html'
+    selector: 'page-matching',
+    templateUrl: 'matching.html'
 })
 export class MatchingPage {
 
-  ready = false;
-  attendants = [];
-  cardDirection = "xy";
-  cardOverlay: any = {
-      like: {
-          backgroundColor: '#28e93b'
-      },
-      dislike: {
-          backgroundColor: '#e92828'
-      }
-  };
+    ready = false;
+    attendants = [];
+    cardDirection = "xy";
+    cardOverlay: any = {
+        like: {
+            backgroundColor: '#28e93b'
+        },
+        dislike: {
+            backgroundColor: '#e92828'
+        }
+    };
 
-  images=["assets/imgs/indy.jpeg","assets/imgs/jax.jpeg", "assets/imgs/boston.jpeg"]
-  constructor(private sanitizer: DomSanitizer) {
+    images = ["assets/imgs/indy.jpeg", "assets/imgs/jax.jpeg", "assets/imgs/boston.jpeg"]
+    constructor(private sanitizer: DomSanitizer) {
 
-      for (let i = 0; i < this.images.length; i++) {
-          this.attendants.push({
-              id: i + 1,
-              likeEvent: new EventEmitter(),
-              destroyEvent: new EventEmitter(),
-              asBg: sanitizer.bypassSecurityTrustStyle('url('+this.images[i]+')')
-          });
-      }
+        for (let i = 0; i < this.images.length; i++) {
+            this.attendants.push({
+                id: i + 1,
+                likeEvent: new EventEmitter(),
+                destroyEvent: new EventEmitter(),
+                asBg: sanitizer.bypassSecurityTrustStyle('url(' + this.images[i] + ')')
+            });
+        }
 
-      this.ready = true;
-  }
+        this.ready = true;
+    }
 
-  onCardInteract(event) {
-    console.log(event);
-  }
+    onCardInteract(event) {
+        //logs like/dislike event for each card
+        console.log(event);
+    }
 
 }
