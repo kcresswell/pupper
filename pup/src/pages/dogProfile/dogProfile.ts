ab626dc080bff05dcfee699e25e16f651c7594c6
kcresswell <makayla.cresswell@gmail.com>
2018-11-21 21:59:28 -0700

    input validation for dogProfile

--- pup/src/pages/dogProfile/dogProfile.ts
+++ pup/src/pages/dogProfile/dogProfile.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { MatchingPage } from '../matching/matching';
 import { TabsPage } from '../tabs/tabs';
 import { AlertController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular'
+
 
 @Component({
   selector: 'page-dogProfile',
@@ -10,16 +12,18 @@ import { AlertController } from 'ionic-angular';
 })
 export class DogProfilePage {
   pupName: string;
-  pupBreed: string; 
-  energyLevel: any; 
-  lifeStage: any; 
-  pupperSex: any; 
+  pupBreed: string;
+  energyLevel: any;
+  lifeStage: any;
+  pupperSex: any;
   pupperNeutered: any;
+  pupBirthdate: Date;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl : AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private toastCtrl: ToastController) {
+
   }
 
-  tab = new TabsPage(); 
+  tab = new TabsPage();
 
   showConfirmAlert() {
     let alertConfirm = this.alertCtrl.create({
@@ -30,7 +34,8 @@ export class DogProfilePage {
           text: 'Find Friendogs!',
           handler: () => {
             console.log('Find Friendogs Clicked');
-            this.navCtrl.push(this.tab.tab2Root);
+            //pup name, breed, life stage, energy level
+            this.navCtrl.push(this.tab.tab2Root, {param1: this.pupName, param2: this.pupBreed, param3: this.lifeStage, param4: this.energyLevel, param5: "assets/imgs/indy.jpeg"});
           }
         }
       ]
@@ -38,25 +43,77 @@ export class DogProfilePage {
     alertConfirm.present();
   }
 
-  createDogProfileBtnClick(){
-      let pupProfileDetails = {
-        pupName: this.pupName,
-        pupBreed: this.pupBreed, 
-        energyLevel: this.energyLevel, 
-        lifeStage: this.lifeStage, 
-        pupperSex: this.pupperSex, 
-        pupperNeutered: this.pupperNeutered
-      };
+  //only allow lowercase letters, uppercase letters, and spaces to be accepted as valid input
+  //returns true if the input is good
+  validateStringInput(strToCheck) {
+    let validStringFormat = /^[a-zA-Z\s]*$/;
+    return (!validStringFormat.test(strToCheck));
+  }
 
-      console.log(pupProfileDetails);
+  //proper date format: MM/DD/YY
+  //returns true if the date is formatted correctly
+  validateDateInput(dateToCheck) {
+    // if(dateToCheck == '') { return false; }
+    let splitDate = dateToCheck.split('/');
+    var date = new Date(splitDate[2] + '/' + splitDate[0] + '/' + splitDate[1]);
+    return (date && (date.getMonth() + 1) == splitDate[0] && date.getDate() == Number(splitDate[1]) && date.getFullYear() == Number(splitDate[2]));
+  }
+
+  presentToast(msgToDisplay) {
+    let toast = this.toastCtrl.create({
+      message: msgToDisplay,
+      duration: 2500,
+      position: 'middle'
+    });
+
+    toast.present();
+  }
+
+
+  createDogProfileBtnClick() {
+    if (this.validateStringInput(this.pupName) || this.validateStringInput(this.pupBreed)) {
+      let errorMsg = "Acceptable Input Limited to Letters and Spaces";
+      console.log(errorMsg);
+      this.presentToast(errorMsg);
+
+      return;
+    }
+
+    //check that a date has been entered and that it is in the proper format
+    if (!this.pupBirthdate || this.validateDateInput(this.pupBirthdate)) {
+      let errorMsg = "Proper Date Format: MM/DD/YYYY";
+      console.log(errorMsg);
+      this.presentToast(errorMsg);
+
+      return;
+    }
+
+    if (!this.energyLevel || !this.lifeStage || !this.pupperSex || !this.pupperNeutered) {
+      let errorMsg = "Please complete entire form";
+      console.log(errorMsg);
+      this.presentToast(errorMsg);
+
+      return;
+    }
+
+    let pupProfileDetails = {
+      pupName: this.pupName,
+      pupBreed: this.pupBreed,
+      energyLevel: this.energyLevel,
+      lifeStage: this.lifeStage,
+      pupperSex: this.pupperSex,
+      pupperNeutered: this.pupperNeutered,
+      pupBirthdate: this.pupBirthdate
+    };
+
+    console.log(pupProfileDetails);
 
     //TODO: Send JSON Request With Dog Profile Data
     //TODO: Have Matching Tab Selected Showing Blue When Nav to this page
     console.log("Create Dog Profile Button Clicked on Dog Profile Page");
 
-    
-    this.showConfirmAlert(); 
+
+    this.showConfirmAlert();
     this.navCtrl.push(TabsPage, {});
   }
 }
-
