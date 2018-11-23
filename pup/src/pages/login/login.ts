7e5c6f76f096b82a00a885206ff03fbf0bd65065
ihyeung <yeung.ireneh@gmail.com>
2018-11-23 04:13:45 -0700

    Found bug with login email validator --regex that is used considers the emails test@@test.com and test@test@test.com as valid email addresses, replaced matcher with improved regex, corrected bug introduced from one of Kaylas recent commits where the loginData JSON object  username field was incorrectly changed to email, implemented new function retrieveUserProfile that occurs after successful login, confirmed that the userProfile corresponding to the email/password a user logs in as is successfully retrieved from the database and the userProfile data is printed to the console.

--- pup/src/pages/login/login.ts
+++ pup/src/pages/login/login.ts
@@ -19,10 +19,105 @@ export class LoginPage {
 
   }
 
-  validateEmail(emailIn) {
+  login() {
+    console.log('Login button clicked');
+
+    if (this.userInputIsValid()) {
+      // let loginData = JSON.stringify({
+      //   username: "test@test.com",
+      //   password: "password"
+      // });
+
+      //Uncomment below for auto filling email/password fields
+      // this.username = "test@test.com";
+      // this.password = "password";
+
+      // let headers = new HttpHeaders({'Content-Type':'application/json'});
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+
+      let loginData = JSON.stringify({
+        username: this.email,
+        password: this.password
+      });
+
+      this.http.post('http://localhost:5000/login', loginData, { headers: headers }) //For running back-end locally
+      // this.http.post('http://pupper.us-east-1.elasticbeanstalk.com/login', loginData, { headers: headers }) //For running back-end in AWS
+      .subscribe(result => {
+        // console.log(result['_body']);
+        console.log('Response status code: ' + result['status']);
+
+        if (result['status'] == 403) {
+          //Invalid credentials
+          let errorMsg = "Invalid login credentials, please try again.";
+          this.presentToast(errorMsg);
+        }
+        else if (result['status'] == 200) {
+          //Success! navigate user to the next page
+          let loginSuccess = "Login success. Please wait . . .";
+          this.presentToast(loginSuccess);
+
+          this.retrieveUserProfile(result);
+
+          //TODO: Make a second update call to userProfile table to update lastLogin for userProfile.
+          this.navCtrl.push(TabsPage);
+          //, {param1: this.email, param2: this.password});
+        }
+        },
+        error => console.log(error)
+      );
+    }
+
+  }
+
+  retrieveUserProfile(response) {
+    let jwtAccessToken = response.headers.get("Authorization");
+
+    console.log(jwtAccessToken);
+
+    let headers = new Headers({'Content-Type':'application/json', 'Authorization': jwtAccessToken});
+
+    console.log(headers);
+    // this.http.get('http://pupper.us-east-1.elasticbeanstalk.com/user', {headers: headers})
+    this.http.get('http://localhost:5000/user?email=' + this.email, {headers: headers})
+    .subscribe(resp => {
+      if (resp['status'] == 403) {
+        this.presentToast("Your session has expired. Please log in again.");
+        return;
+      }
+      else if (resp['status'] == 400 || resp['status'] == 404 || resp['status'] == 422) {
+        let errorMsg = "Error loading User Profile data.";
+        this.presentToast(errorMsg);
+      }
+      else if (resp['status'] == 200) {
+        //TODO: Write logic to appropriately parse and display user profile data on screen.
+        console.log(resp);
+        console.log(resp['_body']);
+      }
+      },
+      error => console.log(error)
+    );
+
+  }
+
+  userInputIsValid(): boolean {
+    if (!this.email || !this.password || !this.validateEmailFormat(this.email)) {
+      let errorMsg = "Please enter a valid email and password.";
+      console.log(errorMsg);
+      this.presentToast(errorMsg);
+
+      return false;
+    }
+    return true;
+  }
+
+  validateEmailFormat(emailIn): boolean {
     //string@string.string
-    let format = /\S+@\S+\.\S+/;
-    return format.test(emailIn);
+    // let format = /\S+@{1}\S+\.\S+/;
+    let emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegEx.test(emailIn)) {
+      console.log("valid email was entered.");
+    }
+    return emailRegEx.test(emailIn);
   }
 
   presentToast(msgToDisplay) {
@@ -33,69 +128,6 @@ export class LoginPage {
     });
 
     toast.present();
-  }
-
-  login() {
-    console.log('Login button clicked');
-
-    // let headers = new HttpHeaders({'Content-Type':'application/json'});
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-
-    // let headers = new Headers({'Content-Type':'application/json', 'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiZXhwIjoxNTQyOTEwNjcwfQ.XQusMM72cqkrGY0oOWGrfbejQxDcDCIGDZoIITtc06xuIXQ59U3CM34IfreVbFlQ8yBdpDYKqH58zUECqbtKHA'});
-
-    // let loginData = JSON.stringify({
-    //   email: "test@test.com",
-    //   password: "password"
-    // });
-
-    //Check if email or password are empty, make sure email is a valid format
-    if (!this.email || !this.password || !this.validateEmail(this.email)) {
-      let errorMsg = "Please enter a valid email and password.";
-      console.log(errorMsg);
-      this.presentToast(errorMsg);
-
-      return;
-
-      //Uncomment below for auto filling email/password fields
-      // this.email = "test@test.com";
-      // this.password = "password";
-    }
-
-    let loginData = JSON.stringify({
-      email: this.email,
-      password: this.password
-    });
-
-    // console.log(loginData);
-
-    //send post request to login
-    this.http.post('http://pupper.us-east-1.elasticbeanstalk.com/login', loginData, { headers: headers })
-      // this.http.get('http://pupper.us-east-1.elasticbeanstalk.com/user', {headers: headers})
-      .subscribe(result => {
-        console.log('response received');
-        console.log(result);
-        // console.log(result['_body']);
-        console.log('Response status code: ' + result['status']);
-        let responseHeaders = result.headers;
-        console.log(responseHeaders);
-        // console.log(responseHeaders['Authorization']);
-
-        if (result['status'] == 403) {
-          //invalid credentials
-          let errorMsg = "Invalid login credentials, please try again.";
-          console.log(errorMsg);
-          this.presentToast(errorMsg);
-        }
-        else if (result['status'] == 200) {
-          //success! navigate user to the next page
-          console.log("Login success");
-          //TODO: send the user account id to the next page so that you display the right profile
-          //TODO: another call to get the user profile from the user account
-          this.navCtrl.push(TabsPage); 
-          //, {param1: this.email, param2: this.password});
-        }
-      });
-
   }
 
 }
