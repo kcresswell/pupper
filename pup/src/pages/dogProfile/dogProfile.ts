5a0247bf3399ef18295dd8fd1cf39816d53dec5a
kcresswell <makayla.cresswell@gmail.com>
2018-12-04 21:35:40 -0700

    updating

--- pup/src/pages/dogProfile/dogProfile.ts
+++ pup/src/pages/dogProfile/dogProfile.ts
@@ -117,17 +117,17 @@ export class DogProfilePage {
 
   // uploadFile POST
   // /upload --> Form Data ProfilePic, requestBody ImageUploadRequest -> MatchProfile
-  // public uploadDogProfilePicFile(headers, headersWithAuthToken, file:Blob, matchProfile, filename) {
-  //   let formData = new FormData();
-  //   formData.append('file', file, filename); //formData.append('file', file, 'test.jpg');
+  public uploadDogProfilePicFile(headers, headersWithAuthToken, file:Blob, matchProfile, filename) {
+    let formData = new FormData();
+    formData.append('file', file, filename); //formData.append('file', file, 'test.jpg');
 
-  //   let uploadDogProfilePicDetails = JSON.stringify({
-  //     formData: this.formData,
-  //     ImageUploadRequest: matchProfile
-  //   });
+    let uploadDogProfilePicDetails = JSON.stringify({
+      formData: this.formData,
+      ImageUploadRequest: matchProfile
+    });
 
-  //   this.http.post('http://pupper.us-east-1.elasticbeanstalk.com/upload', uploadDogProfilePicDetails, { headers: headersWithAuthToken })
-  // }
+    this.http.post('http://pupper.us-east-1.elasticbeanstalk.com/upload', uploadDogProfilePicDetails, { headers: headersWithAuthToken })
+  }
 
   public createDogProfilButtonClickHandler() {
     if (this.userInputIsValid()) {
@@ -177,7 +177,6 @@ public createMatchProfileFromWithBreedObj(breedObj) {
     userProfile: userProfileData
   });
   console.log("MATCHPROFILEDETAILS" + matchProfileDetails);
-  this.globalVarsProvider.setUserMatchProfile(matchProfileDetails); 
 
 
 
@@ -196,7 +195,7 @@ public createMatchProfileFromWithBreedObj(breedObj) {
       let matchProfileCreated = "Match Profile Created! Please wait . . .";
       this.presentToast(matchProfileCreated);
 
-      // this.uploadDogProfilePicFile(headers, headersWithAuthToken, this.profileImage, matchProfileDetails, this.globalVarsProvider.getFileToUpload());
+      this.uploadDogProfilePicFile(headers, headersWithAuthToken, this.profileImage, matchProfileDetails, this.globalVarsProvider.getFileToUpload());
 
       this.navCtrl.push(TabsPage, {matchProfileDetails});
     }
@@ -212,3 +211,217 @@ public createMatchProfileFromWithBreedObj(breedObj) {
 );
 }
 }
+// import { Component } from '@angular/core';
+// import { NavController, NavParams } from 'ionic-angular';
+// import { TabsPage } from '../tabs/tabs';
+// import { AlertController } from 'ionic-angular';
+// import { ToastController } from 'ionic-angular'
+// import { DogProfilePicPage } from '../dogProfilePic/dogProfilePic';
+// import { GlobalvarsProvider } from '../../providers/globalvars/globalvars';
+// // import { internals } from 'rx';
+// import { HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+// import { Http, Response, Headers } from '@angular/http';
+
+
+// @Component({
+//   selector: 'page-dogProfile',
+//   templateUrl: 'dogProfile.html'
+// })
+// export class DogProfilePage {
+//   aboutMe: string;
+//   birthdate: string;
+//   breed: any;
+//   energyLevel: string;
+//   lifeStage: any;
+//   names: string;
+//   numDogs: 1;
+//   profileImage: any;
+//   sex: string;
+//   size: string;
+//   userProfile: any;
+
+//   userId: any;
+//   formData: any;
+
+//   constructor(public navCtrl: NavController, public navParams: NavParams,
+//     public alertCtrl: AlertController, private toastCtrl: ToastController,
+//     public globalVarsProvider: GlobalvarsProvider, public http: Http) {
+//     }
+
+//     //only allow lowercase letters, uppercase letters, and spaces to be accepted as valid input
+//     //returns true if the input is good
+//     validateStringInput(strToCheck) {
+//       let validStringFormat = /^[a-zA-Z\s]*$/;
+//       return (!validStringFormat.test(strToCheck));
+//     }
+
+//     //proper date format: MM/DD/YY
+//     //returns true if the date is formatted correctly
+//     validateDateInput(dateToCheck) {
+//       let splitDate = dateToCheck.split('/');
+//       var date = new Date(splitDate[2] + '/' + splitDate[0] + '/' + splitDate[1]);
+//       return (date && (date.getMonth() + 1) == splitDate[0] && date.getDate() == Number(splitDate[1]) && date.getFullYear() == Number(splitDate[2]));
+//     }
+
+//     presentToast(msgToDisplay) {
+//       let toast = this.toastCtrl.create({
+//         message: msgToDisplay,
+//         duration: 2500,
+//         position: 'middle'
+//       });
+
+//       toast.present();
+//     }
+
+//     addDogProfilePic() {
+//       this.navCtrl.push(DogProfilePicPage, {});
+//     }
+
+//     userInputIsValid() {
+//       if (this.validateStringInput(this.names) || this.validateStringInput(this.breed)) {
+//         let errorMsg = "Acceptable Input Limited to Letters and Spaces";
+//         console.log(errorMsg);
+//         this.presentToast(errorMsg);
+
+//         return false;
+//       }
+
+//       //check that a date has been entered and that it is in the proper format
+//       // if (!this.birthdate || this.validateDateInput(this.birthdate)) {
+//       //   let errorMsg = "Proper Date Format: MM/DD/YYYY";
+//       //   console.log(errorMsg);
+//       //   this.presentToast(errorMsg);
+
+//       //   return false;
+//       // }
+
+//       // if (!this.energyLevel || !this.lifeStage || !this.sex) {
+//       //   let errorMsg = "Please complete entire form";
+//       //   console.log(errorMsg);
+//       //   this.presentToast(errorMsg);
+
+//       //   return false;
+//       // }
+//       return true;
+//     }
+
+//     createDogProfileBtnClick() {
+//       this.createDogProfilButtonClickHandler();
+//       console.log("Create Dog Profile Button Clicked on Dog Profile Page");
+//     }
+
+//     //GET /pupper
+//     findPupperBreedByName(headers, headersWithAuthToken){
+//       let breed;
+//       this.http.get('http://pupper.us-east-1.elasticbeanstalk.com/pupper?breed=' + this.breed, {headers: headersWithAuthToken})
+//       .subscribe(result => {
+//         console.log('Response status code: ' + result['status']);
+//         if (result['status'] == 200) {
+//           console.log(result);
+//           let jsonObj = JSON.parse((result['_body']));
+//           breed = jsonObj.name;
+//           console.log("Breed: " + breed);
+//         }
+//       },
+//       error => console.log(error)
+//     );
+//     return breed;
+//   }
+
+//   // uploadFile POST
+//   // /upload --> Form Data ProfilePic, requestBody ImageUploadRequest -> MatchProfile
+//   // public uploadDogProfilePicFile(headers, headersWithAuthToken, file:Blob, matchProfile, filename) {
+//   //   let formData = new FormData();
+//   //   formData.append('file', file, filename); //formData.append('file', file, 'test.jpg');
+
+//   //   let uploadDogProfilePicDetails = JSON.stringify({
+//   //     formData: this.formData,
+//   //     ImageUploadRequest: matchProfile
+//   //   });
+
+//   //   this.http.post('http://pupper.us-east-1.elasticbeanstalk.com/upload', uploadDogProfilePicDetails, { headers: headersWithAuthToken })
+//   // }
+
+//   public createDogProfilButtonClickHandler() {
+//     if (this.userInputIsValid()) {
+//       //get user details from globalvars.ts
+//       let userProfileData = this.globalVarsProvider.getUserProfileData();
+//       this.userId = this.globalVarsProvider.getUserId();
+
+//       const headers = new Headers({ 'Content-Type': 'application/json' });
+//       let headersWithAuthToken = this.globalVarsProvider.getHeadersWithAuthToken();
+
+//       // let breedResponse = this.findPupperBreedByName(headers, headersWithAuthToken);
+//       this.http.get('http://pupper.us-east-1.elasticbeanstalk.com/pupper?breed=' + this.breed, {headers: headersWithAuthToken})
+//       .subscribe(result => {
+//         console.log('Response status code: ' + result['status']);
+//         if (result['status'] == 200) {
+//           console.log(result);
+//           // let jsonObj = JSON.parse((result['_body']));
+//           let breedResponse = JSON.parse(result['_body']);
+//           // breed = jsonObj.name;
+//           this.createMatchProfileFromWithBreedObj(breedResponse);
+//         }
+//       },
+//       error => console.log(error)
+//     );
+//   }
+// }
+
+// public createMatchProfileFromWithBreedObj(breedObj) {
+
+//   let userProfileData = this.globalVarsProvider.getUserProfileData();
+//   this.userId = this.globalVarsProvider.getUserId();
+
+//   const headers = new Headers({ 'Content-Type': 'application/json' });
+//   let headersWithAuthToken = this.globalVarsProvider.getHeadersWithAuthToken();
+
+//   let matchProfileDetails = JSON.stringify({
+//     aboutMe: this.aboutMe,
+//     birthdate: "2017-08-31", //this.birthdate format being wonky, TODO: grab value from user
+//     breed: breedObj,
+//     energyLevel: "HIGH", //hardcoded value for now
+//     lifeStage: this.lifeStage,
+//     names: this.names,
+//     numDogs: 1,
+//     profileImage: null,
+//     sex: this.sex,
+//     size: this.size,
+//     userProfile: userProfileData
+//   });
+//   console.log("MATCHPROFILEDETAILS" + matchProfileDetails);
+//   this.globalVarsProvider.setUserMatchProfile(matchProfileDetails); 
+
+
+
+//   // createMatchProfileForUserByUserProfileId -- POST /user/{userId}/matchProfile
+//   // this.http.post('http://localhost:5000/matchProfile', matchProfileDetails, { headers: headersWithAuthToken }) //For running back-end in AWS
+//   this.http.post('http://pupper.us-east-1.elasticbeanstalk.com/user/' + this.userId +'/matchProfile', matchProfileDetails, { headers: headersWithAuthToken }) //For running back-end in AWS
+//   .subscribe(result => {
+//     console.log(result);
+//     console.log(result['_body']);
+//     console.log('Response status code: ' + result['status']);
+//     if (result['status'] == 200) {
+//       console.log(result);
+//       let jsonResponseObj = JSON.parse((result['_body']));
+
+//       //Success! navigate user to the next page
+//       let matchProfileCreated = "Match Profile Created! Please wait . . .";
+//       this.presentToast(matchProfileCreated);
+
+//       // this.uploadDogProfilePicFile(headers, headersWithAuthToken, this.profileImage, matchProfileDetails, this.globalVarsProvider.getFileToUpload());
+
+//       this.navCtrl.push(TabsPage, {matchProfileDetails});
+//     }
+//     else if (result['status'] == 400 || result['status'] == 404) {
+//       //REMOVE THIS LATER
+//       this.presentToast("There's an error with one of your matchProfile fields, this status code should never happen.");
+//     }
+//   },
+//   error => {
+//     console.log("ERROR: " + error);
+//     console.log(error['_body']);
+//   }
+// );
+// }
+// }
