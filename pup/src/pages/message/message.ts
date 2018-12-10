658f82c6558d141e64f890a73d88f5a53075927f
ihyeung <yeung.ireneh@gmail.com>
2018-12-10 14:56:17 -0700

    cleaned up junk and added correct logic to check if a message was sent successfully

--- pup/src/pages/message/message.ts
+++ pup/src/pages/message/message.ts
@@ -21,26 +21,32 @@ export class MessagePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController,
     public http: Http, public globalVarsProvider: GlobalvarsProvider) {
-    this.matchProfileReceiver = {
-      "aboutMe": "Cute dog.", "birthdate": "2017-12-06", "breed": { "id": 10, "name": "Shiba Inu", "altName": "Shiba", "size": "SMALL" },
-      "energyLevel": "LOW", "lifeStage": "YOUNG", "names": "Indy", "numDogs": 1, "profileImage": null, "sex": "MALE", "size": "MID",
-      "userProfile": {
-        "id": 1, "userAccount": { "id": 1, "username": "test@test.com", "password": "$2a$04$l1TV6PnY3kg0xtQBjQpgYeIbPttgHLotrOKMLPOPvJk4s304vUPIu" }, "firstName": "Bob", "lastName": "Smith", "sex":
-          "MALE", "birthdate": "1992-04-10", "maritalStatus": "SINGLE", "zip": "84095", "dateJoin": "2018-11-16", "lastLogin": "2018-12-05", "profileImage": "https://s3.us-east-1.amazonaws.com/pupper-mobile-app/user_1_bob_2018-12-03T03:02:36Z"
-      }
-    }
+
+    //WHATEVER THIS this.matchProfileReceiver OBJECT IS IS JUNK AND NOT IN THE RIGHT FORMAT THAT A MATCH PROFILE SHOULD BE.
+
+    // this.matchProfileReceiver = {
+    //   "aboutMe": "Cute dog.", "birthdate": "2017-12-06", "breed": { "id": 10, "name": "Shiba Inu", "altName": "Shiba", "size": "SMALL" },
+    //   "energyLevel": "LOW", "lifeStage": "YOUNG", "names": "Indy", "numDogs": 1, "profileImage": null, "sex": "MALE", "size": "MID",
+    //   "userProfile": {
+    //     "id": 1, "userAccount": { "id": 1, "username": "test@test.com", "password": "$2a$04$l1TV6PnY3kg0xtQBjQpgYeIbPttgHLotrOKMLPOPvJk4s304vUPIu" }, "firstName": "Bob", "lastName": "Smith", "sex":
+    //       "MALE", "birthdate": "1992-04-10", "maritalStatus": "SINGLE", "zip": "84095", "dateJoin": "2018-11-16", "lastLogin": "2018-12-05", "profileImage": "https://s3.us-east-1.amazonaws.com/pupper-mobile-app/user_1_bob_2018-12-03T03:02:36Z"
+    //   }
+    // }
+
   }
 
   onSendBtnClick() {
+    const senderUserProfileId = 2;
+    const receiverUserProfileId = 3;
     console.log("onSendBtnClick clicked to send message");
     console.log("Message: " + this.message);
-    const senderId = this.globalVarsProvider.getUserId();
-    this.retrieveMatchProfilesAndSendMessage(2);
+    const senderId = this.globalVarsProvider.getUserId(); //THIS IS UNDEFINED
+    this.retrieveMatchProfilesAndSendMessage(senderUserProfileId, receiverUserProfileId);
     this.message = "HELLO MESSAGES WORKING!";
   }
 
-  retrieveMatchProfilesAndSendMessage(userProfileId) {
-  const getMatchProfileEndpointUrl = this.globalVarsProvider.getServerBaseUrl() + "/user/" + userProfileId + "/matchProfile";
+  retrieveMatchProfilesAndSendMessage(senderUserProfileId, receiverUserProfileId) {
+  const getMatchProfileEndpointUrl = this.globalVarsProvider.getServerBaseUrl() + "/user/" + senderUserProfileId + "/matchProfile";
 
   console.log("Hitting endpoint to retrieve match profile for a given user id: " + getMatchProfileEndpointUrl);
   this.http.get(getMatchProfileEndpointUrl, { headers: this.globalVarsProvider.getHeadersWithAuthToken()})
@@ -50,7 +56,7 @@ export class MessagePage {
           let jsonResponseObj = JSON.parse((response['_body']));
                 let matchProfileObj = jsonResponseObj['matchProfiles'][0];
                 this.globalVarsProvider.setUserMatchProfile(matchProfileObj);
-                this.retrieveMatchProfileReceiver(3);
+                this.retrieveMatchProfileReceiver(receiverUserProfileId);
         }
       },
         error => console.log(error)
@@ -62,7 +68,6 @@ export class MessagePage {
     console.log("Hitting endpoint to retrieve match profile for a given user id: " + getMatchProfileEndpointUrl);
     this.http.get(getMatchProfileEndpointUrl, { headers: this.globalVarsProvider.getHeadersWithAuthToken()})
       .subscribe(response => {
-
           if (response['status'] == 200) {
             let jsonResponseObj = JSON.parse((response['_body']));
                   let matchProfileObj = jsonResponseObj['matchProfiles'][0];
@@ -75,70 +80,39 @@ export class MessagePage {
         );
   }
 
-  //sendMessageToMatch --- POST /message --- Query - sendFrom | Query - sendTo | Body - pupperMessage
   sendMessageToMatch(matchProfileReceiverObj) {
-    // this.sendFrom = this.globalVarsProvider.getUserMatchProfile()['id'];
-    // if(this.sendFrom == Object){
-    //   let errorMsg = "Please create a profile for your pupper before matching. This can be added on the Settings tab.";
-    //   this.presentToast(errorMsg);
-    // }
-
+    //The following fields need to be parsed from their matchProfileObjects,
+    //i've hardcoded them for now for the sake of time
     this.sendFrom = 2;
-    console.log("SendFrom: " + this.sendFrom);
+    this.sendTo = 3;
 
-    console.log("matchProfileSender object: " + this.globalVarsProvider.getUserMatchProfile());
-    this.sendTo = 3; //userProfileId
     let pupperMessageBody = JSON.stringify({
       matchProfileReceiver: matchProfileReceiverObj,
       matchProfileSender: this.globalVarsProvider.getUserMatchProfile(),
       message: this.message,
       timestamp: null
     });
+    // 
+    // //for displaying the message to the screen - move to 200 code block when it is working
+    // this.allMessages.push(this.message);
+    // for (let i = 0; i < this.allMessages.length; i++) {
+    //   this.messageToSend.push({
+    //     message: this.message
+    //   });
+    // }
 
-    //for displaying the message to the screen - move to 200 code block when it is working
-    this.allMessages.push(this.message);
-    for (let i = 0; i < this.allMessages.length; i++) {
-      this.messageToSend.push({
-        message: this.message
-      });
-    }
-    //
-    // let messageBody = JSON.stringify({
-    //   sendFrom: this.sendFrom,
-    //   sendTo: this.sendTo,
-    //   pupperMessage: pupperMessageBody
-    // });
+    const sendMessageUrl = this.globalVarsProvider.getServerBaseUrl() + "/message?sendFrom=" +
+                            this.sendFrom + "&sendTo=" + this.sendTo;
 
-    console.log("Message Body: " + pupperMessageBody);
-    // [Log] Message Body: {"sendTo":1,"pupperMessage":{"matchProfileReceiver":{"aboutMe":"Cute dog.","birthdate":"2017-12-06",
-    // "breed":{"id":10,"name":"Shiba Inu","altName":"Shiba","size":"SMALL"},"energyLevel":"LOW","lifeStage":"YOUNG",
-    // "names":"Indy","numDogs":1,"profileImage":null,"sex":"MALE","size":"MID","userProfile":{"id":1,"userAccount":
-    // {"id":1,"username":"test@test.com","password":"$2a$04$l1TV6PnY3kg0xtQBjQpgYeIbPttgHLotrOKMLPOPvJk4s304vUPIu"},
-    // "firstName":"Bob","lastName":"Smith","sex":"MALE","birthdate":"1992-04-10","maritalStatus":"SINGLE","zip":"84095",
-    // "dateJoin":"2018-11-16","lastLogin":"2018-12-05","profileImage":"https://s3.us-east-1.amazonaws.com/pupper-mobile-app/user_1_bob_2018-12-03T03:02:36Z"}}
-    // ,"message":"Hello"}} (main.js, line 441)
-
-    // const headers = new Headers({ 'Content-Type': 'application/json' });
-    const sendMessageUrl = this.globalVarsProvider.getServerBaseUrl() + "/message?sendFrom=" + this.sendFrom + "&sendTo=" + this.sendTo;
-    console.log("Sending message endpoint: " + sendMessageUrl);
     this.http.post(sendMessageUrl, pupperMessageBody,
-    { headers: this.globalVarsProvider.getHeadersWithAuthToken() })
+          { headers: this.globalVarsProvider.getHeadersWithAuthToken() })
       .subscribe(response => {
-        console.log(response['_body']);
-        console.log('Response status code: ' + response['status']);
+        const jsonResponseObj = JSON.parse((response['_body']));
+        if (jsonResponseObj['isSuccess'] == 200) {
+          console.log("Message successfully sent.");
+        }
 
-        if (response['status'] == 200) {
-          //message sent
-        }
-        else if (response['status'] == 403) {
-          //TODO: add a popup here for session expiration, button goes back to login page
-        }
-        else {
-          let errorMsg = "Something went wrong trying to send the message, please try again.";
-          this.presentToast(errorMsg);
-        }
-      },
-        error => console.log(error)
+      }, error => console.log(error)
       );
   }
 
