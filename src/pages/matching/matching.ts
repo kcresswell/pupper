54dae78575b257c3ac90fd970584749762b82e2b
ihyeung <yeung.ireneh@gmail.com>
2018-12-12 22:55:09 -0700

    added necessary logic to appropriately display an error message if upon login an invalid password is entered with an existing valid email (previously failed to display an error message unless an invalid email was entered), added conditional check to see if lastLogin update http call needs to be made, cleaned up extraneous code in login.ts, deleted redundant pup directory folder

---
+++ src/pages/matching/matching.ts
@@ -0,0 +1,91 @@
+import { Component, EventEmitter } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AlertController } from 'ionic-angular';
+import { MessagePage } from '../message/message';
+
+@Component({
+    selector: 'page-matching',
+    templateUrl: 'matching.html'
+})
+export class MatchingPage {
+    aboutMe: string;
+    ageWithUnits: string;
+    breedName: string;
+    distance: string;
+    lastActive: string;
+    location: string;
+    name: string;
+    profileId: any;
+    profileImage: string;
+    sex: string;
+    matchProfileDetails: any;
+    profileCard: any;
+
+    ready = false;
+    attendants = [];
+    cardDirection = "xy";
+    cardOverlay: any = {
+        like: {
+            backgroundColor: '#28e93b'
+        },
+        dislike: {
+            backgroundColor: '#e92828'
+        }
+    };
+
+
+    constructor(private sanitizer: DomSanitizer, public navParams: NavParams, public alertCtrl: AlertController,
+        public navCtrl: NavController) {
+        this.grabCards();
+    }
+
+    grabCards() {
+        let images = ["assets/imgs/indy.jpeg", "assets/imgs/jax.jpg", "assets/imgs/boston.jpeg", "assets/imgs/beagle.jpeg",
+            "assets/imgs/chihua.jpeg", "assets/imgs/collie.jpeg", "assets/imgs/doodle.jpeg", "assets/imgs/maltese.jpeg", "assets/imgs/sheltie.jpeg"]
+
+        let pupInfo = [new Array("Indy", " Shiba Inu", " Female"), new Array("Jax", " Pomeranian", " Male"), new Array("Boston", " Shiba Inu", " Male")];
+
+        for (let i = 0; i < images.length; i++) {
+            this.profileCard = pupInfo[0];
+            this.attendants.push({
+                id: i + 1,
+                likeEvent: new EventEmitter(),
+                destroyEvent: new EventEmitter(),
+                asBg: this.sanitizer.bypassSecurityTrustStyle('url(' + images[i] + ')')
+            });
+        }
+        this.ready = true;
+    }
+
+    onCardInteract(event) {
+        if (event.like) {
+            this.popupSendAMessageQuestion();
+        }
+        console.log(event);
+    }
+
+    popupSendAMessageQuestion() {
+        let alertConfirm = this.alertCtrl.create({
+            title: 'It\'s a match!',
+            message: 'Would you like to send this pup a message?',
+            buttons: [
+                {
+                    text: 'Continue',
+                    handler: () => {
+                        console.log('Continue Clicked');
+                    }
+                },
+                {
+                    text: 'Let\'s Chat!',
+                    handler: () => {
+                        console.log('Lets Chat Clicked');
+                        console.log("Match profile details from matching page " + this.matchProfileDetails);
+                        this.navCtrl.push(MessagePage, { matchProfileDetails: this.matchProfileDetails });
+                    }
+                }
+            ]
+        });
+        alertConfirm.present();
+    }
+}
---
+++ src/providers/globalvars/globalvars.ts
@@ -0,0 +1,90 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the GlobalvarsProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class GlobalvarsProvider {
+
+  serverBaseUrl: any;
+  fileToUpload: any;
+  filename: any;
+  userId: any;
+  userProfileData: any;
+  headersWithAuthToken: any;
+  userMatchProfile: any;
+  userMatchProfileId: any; 
+
+  constructor(public http: HttpClient) {
+    this.setServerBaseUrl();
+  }
+
+  public setServerBaseUrl() {
+    this.serverBaseUrl = "http://pupper.us-east-1.elasticbeanstalk.com";
+  }
+
+  public getServerBaseUrl() {
+    return this.serverBaseUrl;
+  }
+
+  public setFileToUpload(value) {
+    this.fileToUpload = value;
+    console.log("Global Vars, File To Upload: " + this.fileToUpload);
+  }
+
+  public getFileToUpload() {
+    return this.fileToUpload;
+  }
+
+  public setFilename(value) {
+    this.filename = value;
+  }
+
+  public getFilename() {
+    return this.filename;
+  }
+
+  public setUserId(value) {
+    this.userId = value;
+  }
+
+  public getUserId() {
+    return this.userId;
+  }
+
+  public setUserProfileData(value) {
+    this.userProfileData = value;
+  }
+
+  public getUserProfileData() {
+    return this.userProfileData;
+  }
+
+  public setUserMatchProfile(value) {
+    this.userMatchProfile = value;
+  }
+
+  public getUserMatchProfile() {
+    return this.userMatchProfile;
+  }
+
+  public setUserMatchProfileId(value) {
+    this.userMatchProfileId = value;
+  }
+
+  public getUserMatchProfileId() {
+    return this.userMatchProfileId;
+  }
+
+  public setHeadersWithAuthToken(value) {
+    this.headersWithAuthToken = value;
+  }
+
+  public getHeadersWithAuthToken() {
+    return this.headersWithAuthToken;
+  }
+}
